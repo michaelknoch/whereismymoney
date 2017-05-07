1019856dcac2f39900872e0612ec427dc4d5f257
Michael Knoch <info@michaelknoch.de>
2017-05-07 15:58:30 +0200

    group data by category

--- app/calculate.js
+++ app/calculate.js
@@ -1,21 +1,69 @@
 
 export default function (data) {
-    const calculated = {};
+    return groupDataByCategory(groupDataByCompany(data));
+}
+
+function groupDataByCompany(data) {
+    const groupedData = {};
 
     data.forEach((elem) => {
-        if (elem.Betrag > 0) { return; }
+        if (elem.Betrag > 0 || !elem['Beguenstigter/Zahlungspflichtiger']) { return; }
 
         const recipient = elem['Beguenstigter/Zahlungspflichtiger'];
-        if (calculated[recipient]) {
-            calculated[recipient] = {
-                total: calculated[recipient].total + elem.Betrag,
+        if (groupedData[recipient]) {
+            groupedData[recipient] = {
+                ...groupedData[recipient],
+                total: groupedData[recipient].total + elem.Betrag,
             };
         } else {
-            calculated[recipient] = {
+            groupedData[recipient] = {
                 ...elem,
                 total: elem.Betrag,
             };
         }
     });
-    return calculated;
+    return groupedData;
+}
+
+export function groupDataByCategory(data) {
+    const groupedData = {};
+
+    Object.keys(data).forEach((key) => {
+        const object = data[key];
+        const group = getCategoryByCompany(object['Beguenstigter/Zahlungspflichtiger']);
+        if (groupedData[group]) {
+            groupedData[group] = {
+                total: groupedData[group].total + object.total,
+                detail: [...groupedData[group].detail, object],
+            };
+        } else {
+            groupedData[group] = {
+                total: object.total,
+                detail: [object],
+            };
+        }
+    });
+    return groupedData;
+}
+
+
+export function getCategoryByCompany(string = '') {
+    const categories = {
+        lebensmittel: ['edeka', 'kaufland', 'rewe', 'lidl', 'aldi'],
+        wohnung: ['ikea, depot', 'butlers'],
+        fix: ['Telefonica Germany', 'BVG', 'Michael Knoch'],
+        outfit: ['KARSTADT', 'H&M', 'Pull&Bear', 'zalando'],
+        paypal: ['PayPal'],
+        amazon: ['amazon'],
+        sparkasse: ['sparkasse'],
+        kreditkarte: ['KREDITKARTENABRECHNUNG'],
+    };
+
+    const keys = Object.keys(categories);
+    for (const key of keys) {
+        for (const brand of categories[key]) {
+            if (string.toLowerCase().includes(brand.toLowerCase())) return key;
+        }
+    }
+    return string;
 }
